6dacf32cd0928e30e9a1381b24f6ba0113750ed9
Elder453 <elder.veliz@yale.edu>
2025-01-29 23:52:06 -0500

    update init

--- kuramoto/__init__.py
+++ kuramoto/__init__.py
@@ -2,6 +2,7 @@ from kuramoto.kuramoto import Kuramoto
 from kuramoto.plotting import plot_activity, plot_phase_coherence
 #from kuramoto.generalized_kuramoto import GeneralizedKuramoto
 from kuramoto.gkm import GeneralizedKuramoto
+from kuramoto.gkm_gpu import GPUKuramoto
 
 __version__ = "0.3.0"
 
